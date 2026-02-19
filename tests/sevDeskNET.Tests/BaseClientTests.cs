8ed875d01f1290b05cd9afdafff016095f57968f
Timm Bremekamp <tb@fm-gruppe.com>
2026-02-19 09:46:30 +0100

    Add BaseClient.ParseFactoryResponseId edge case tests
    Test numeric ID, string ID, missing property and invalid ID
    parsing to cover all branches in the factory response parser.

---
+++ tests/sevDeskNET.Tests/BaseClientTests.cs
@@ -0,0 +1,47 @@
+using sevDeskNET.Exceptions;
+using sevDeskNET.Internal;
+using Shouldly;
+using Xunit;
+
+namespace sevDeskNET.Tests;
+
+public class BaseClientTests
+{
+    [Fact]
+    public void ParseFactoryResponseId_NumericId_ReturnsInt()
+    {
+        var json = """{"objects":{"invoice":{"id":42}}}""";
+
+        var result = BaseClient.ParseFactoryResponseId(json, "invoice");
+
+        result.ShouldBe(42);
+    }
+
+    [Fact]
+    public void ParseFactoryResponseId_StringId_ReturnsInt()
+    {
+        var json = """{"objects":{"invoice":{"id":"99"}}}""";
+
+        var result = BaseClient.ParseFactoryResponseId(json, "invoice");
+
+        result.ShouldBe(99);
+    }
+
+    [Fact]
+    public void ParseFactoryResponseId_MissingProperty_Throws()
+    {
+        var json = """{"objects":{}}""";
+
+        Should.Throw<SevDeskApiException>(
+            () => BaseClient.ParseFactoryResponseId(json, "invoice"));
+    }
+
+    [Fact]
+    public void ParseFactoryResponseId_InvalidId_Throws()
+    {
+        var json = """{"objects":{"invoice":{"id":"not-a-number"}}}""";
+
+        Should.Throw<SevDeskApiException>(
+            () => BaseClient.ParseFactoryResponseId(json, "invoice"));
+    }
+}
